47ab08ccb42389e09f0311157b2f27e3532f3d0e
chrisfoong <chris.owo@hotmail.com>
2025-04-26 15:23:59 +0700

    remove API key

--- libs/api.ts
+++ libs/api.ts
@@ -1,6 +1,6 @@
 
 export async function fetchWeather(city:string) {
-  const api_key = "080a30ba08a5db5fbb7244fed26c611f";
+  const api_key = "";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 
   await new Promise((resolve) => setInterval(resolve, 1500));
